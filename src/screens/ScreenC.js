23c2218dbfc2b2f3cbe5f1a1d38cf639e328b57b
olios <phumai@bluebik.com>
2021-10-19 10:57:21 +0700

    add ui-components

--- src/screens/ScreenC.js
+++ src/screens/ScreenC.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { SafeAreaView, Text, View, Button } from "react-native";
+import { UiButton } from "@test-superapp/ui-components";
 
 function ScreenC({ navigation }) {
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>MiniApp2 ScreenC</Text>
-      <Button
+      <Text>Version 0.0.3</Text>
+      <UiButton
         title="Go to ScreenD"
         onPress={() => navigation.navigate('ScreenD')}
       />
--- src/screens/ScreenD.js
+++ src/screens/ScreenD.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { SafeAreaView, Text, View, Button } from "react-native";
+import { UiButton } from "@test-superapp/ui-components";
 
 function ScreenD({ navigation }) {
   return (
     <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>MiniApp 2 ScreenD</Text>
-      <Button
+      <UiButton
         title="Go to MiniApp1"
         onPress={() => navigation.navigate('MiniApp1')}
       />
